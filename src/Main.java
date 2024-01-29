89c21eb788281910af2a84f730e567fb1e69b0f4
sandy.babineau24 <sandybabineau64@gmail.com>
2024-01-29 08:26:41 -0600

    hiiiii

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+class Main
+{
+    public static void main(String[] args){
+
+        JFrame frame = new JFrame();
+        frame.setSize(400,400);
+
+        Panel panel = new Panel();
+        panel.setBackground(Color.PINK);
+       panel.setLayout(new GridLayout(2,2));
+
+        JLabel label = new JLabel("What animal is this?");
+        JButton button = new JButton("Check me");
+        JTextField textField = new JTextField(10);
+
+        ImageIcon icon = new ImageIcon("images/PNG/2C.png");
+        icon.setImage(icon.getImage().getScaledInstance(50,50,Image.SCALE_DEFAULT));
+        JLabel picLabel = new JLabel(icon);
+
+        panel.add(picLabel);
+        panel.add(label);
+       panel.add(textField);
+        panel.add(button);
+
+        frame.add(panel);
+
+        frame.setVisible(true);
+
+    }
+}
